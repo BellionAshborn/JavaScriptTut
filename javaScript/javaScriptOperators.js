9ea2a83ff609971246963bb90fb498039f5f51b9
BellionAshborn <160540915+BellionAshborn@users.noreply.github.com>
2025-07-07 23:21:12 +0530

    Update javaScriptOperators.js

--- javaScript/javaScriptOperators.js
+++ javaScript/javaScriptOperators.js
@@ -13,13 +13,13 @@ var b = a++;
 console.log(b); //  here it will be 10 and will not change to 11 
 console.log(a); //  here it will be changed to 11 
 
-// Pre-Increment --> -
+// Pre-Decrement --> -
 
 var a = 10; 
 var b = --a;
 console.log(b); //  here it will be changed to 9
 
-// Post-Increment --> -
+// Post-Decrement --> -
 
 var a = 10; 
 var b = a--;    
